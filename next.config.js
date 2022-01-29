81f227e8079e378248abeb95b73f4ff155bcc9a6
Helena Oliveira <helena@informach.com.br>
2022-01-29 10:42:28 -0300

    finish config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = withPWA({
   reactStrictMode: true,
-}
+  swcMinify: true,
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true
+  },
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
 
 module.exports = nextConfig
